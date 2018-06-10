25f8ac79b80d8d885402a73331103dab5282a207
Abdul Dakkak <abduld@wolfram.com>
2018-06-10 19:33:54 +0100

    updates

--- microbench_plot/plot.py
+++ microbench_plot/plot.py
@@ -2,28 +2,40 @@
 
 import json
 import sys
-import prettyprinter as pp
+import pprint
 import os
 import re
 import numpy as np
 import matplotlib.pyplot as plt
 import yaml
 import seaborn as sns
-
-from .configure_plot import *
+import pandas as pd
+from matplotlib.ticker import ScalarFormatter, AutoMinorLocator
+import matplotlib as mpl
 
 pp = pprint.PrettyPrinter(indent=4)
 
-def generator_errorbar(fig, yaml_dir, plot_cfg):
-    ax = fig.add_subplot(111)
-    
-    for s in plot_cfg["series"]:
-        file_path = s["file"]
+def generator_bar(fig, yaml_dir, plot_cfg):
+    ax = fig.add_subplot(1, 1, 1)
+    bar_width = plot_cfg.get("bar_width", 0.8)
+    num_series = len(plot_cfg["series"])
+
+    default_file = plot_cfg.get("file", "not_found")
+
+    default_x_scale =  eval(str(plot_cfg.get("xaxis", {}).get("scale", 1.0)))
+    default_y_scale =  eval(str(plot_cfg.get("yaxis", {}).get("scale", 1.0)))
+
+    default_x_field = plot_cfg.get("xaxis", {}).get("field", "real_time")
+    default_y_field = plot_cfg.get("yaxis", {}).get("field", "real_time")
+
+    for c, s in enumerate(plot_cfg["series"]):
+        file_path = s.get("file", default_file)
         label = s["label"]
+        print(label)
         regex = s.get("regex", ".*")
         print("Using regex:", regex)
-        yscale = float(s.get("yscale", 1.0))
-        xscale = float(s.get("xscale", 1.0))
+        yscale = eval(str(s.get("yscale", default_y_scale)))
+        xscale = eval(str(s.get("xscale", default_x_scale)))
         if not os.path.isabs(file_path):
             file_path = os.path.join(yaml_dir, file_path)
         print("reading", file_path)
@@ -32,20 +44,29 @@ def generator_errorbar(fig, yaml_dir, plot_cfg):
         
         pattern = re.compile(regex)
         matches = [b for b in j["benchmarks"] if pattern == None or pattern.search(b["name"])]
-        means = [b for b in matches if b["name"].endswith("_mean")]
-        stddevs = [b for b in matches if b["name"].endswith("_stddev")]
-        x = np.array([float(b["bytes"]) for b in means])
-        y = np.array([float(b["bytes_per_second"]) for b in means])
-        e = np.array([float(b["bytes_per_second"]) for b in stddevs])
+        times = matches
+
+        if len(times) == 0:
+            continue
+
+        xfield = s.get("xfield", default_x_field)
+        x = np.array([float(b[xfield]) for b in times])
+
+        yfield = s.get("yfield", default_y_field)
+        y = np.array([float(b[yfield]) for b in times])
 
         # Rescale
         x *= xscale
         y *= yscale
-        e *= yscale
 
-        # pp.pprint(means)
+        # pp.pprint(y)
 
-        ax.errorbar(x, y, e, capsize=3, label=label)
+        # ax.bar(x + (c - num_series/2) * bar_width, y, width=bar_width, label=label, align='center')
+        ax.bar(x + bar_width*c, y, width=bar_width, label=label, align='center')
+        ax.set_xticks(x + 1.5 * bar_width)
+        if c == 0:
+            ax.set_xticklabels((x + c*bar_width).round(1))
+        # ax.bar(x , y, width=bar_width, label=label, align='center')
 
     if "yaxis" in plot_cfg:
         axis_cfg = plot_cfg["yaxis"]
@@ -60,8 +81,12 @@ def generator_errorbar(fig, yaml_dir, plot_cfg):
 
     if "xaxis" in plot_cfg:
         axis_cfg = plot_cfg["xaxis"]
-        if axis_cfg and "scale" in axis_cfg:
-            scale = axis_cfg["scale"]
+        if axis_cfg and "lim" in axis_cfg:
+            lim = axis_cfg["lim"]
+            print("setting xlim", lim)
+            ax.set_xlim(lim)
+        if axis_cfg and "scaling_function" in axis_cfg:
+            scale = axis_cfg["scaling_function"]
             print("setting xscale", scale)
             ax.set_xscale(scale, basex=2)
         if axis_cfg and "label" in axis_cfg:
@@ -74,115 +99,63 @@ def generator_errorbar(fig, yaml_dir, plot_cfg):
         print("setting title", title)
         ax.set_title(title)
 
-    ax.legend()
+    # ax.legend(loc='upper left')
+    ax.legend(loc='best')
 
     return fig
 
-def generator_regplot(fig, yaml_dir, plot_cfg):
-
-    ax = fig.add_subplot(111)
-
-    series_cfgs = plot_cfg["series"]
-    for s_cfg in series_cfgs:
-        file_path = s_cfg["file"]
-        label = s_cfg["label"]
-        regex = s_cfg.get("regex", ".*")
-        print("Using regex:", regex)
-        if not os.path.isabs(file_path):
-            file_path = os.path.join(yaml_dir, file_path)
-        print("reading", file_path)
-        with open(file_path, "rb") as f:
-            j = json.loads(f.read().decode('utf-8'))
-        
-        pattern = re.compile(regex)
-        matches = [b for b in j["benchmarks"] if pattern == None or pattern.search(b["name"])]
-        means = [b for b in matches if b["name"].endswith("_mean")]
-        stddevs = [b for b in matches if b["name"].endswith("_stddev")]
-        x = np.array([int(b["bytes"]) for b in means])
-        y = np.array([float(b["bytes_per_second"]) for b in means])
-        e = np.array([float(b["bytes_per_second"]) for b in stddevs])
-
-        color = s_cfg.get("color", "black")
-        style = s_cfg.get("style", "-")
-
-        ax = sns.regplot(ax=ax, x=x_col, y=col, data=df,
-                         ci=68, label=label, color=color, line_kws={"linestyle": style})
-
-    # Set limits
-    ax.set_ylim([0, 1600])
-    ylabel = plot_cfg.get("yaxis", {}).get("label", "")
-    print ("set ylabel to:", ylabel)
-    ax.set_ylabel(ylabel)
-
-    # update labels
-    h, l = ax.get_legend_handles_labels()
-    for s_cfg in enumerate(series_cfgs):
-        # def get_linregress(series):
-        #     return scipy.stats.linregress(x=ax.get_lines()[series].get_xdata(), y=ax.get_lines()[series].get_ydata())
-        def get_polyfit(series):
-            x = ax.get_lines()[series].get_xdata()
-            y = ax.get_lines()[series].get_ydata()
-            z, cov = np.polyfit(x, y, 1, cov=True)
-            # print np.sqrt(np.diag(cov))
-            return z[0], z[1]
-        # slope, intercept, r_value, p_value, std_err = get_linregress(c)
-        slope, intercept = get_polyfit(c)
-        l[c] = l[c] + ": " + "{:.2f}".format(slope) + " us/fault"
-        print ("set label", c, "to:", l[c])
-    ax.legend(h, l)
-    ax.legend(frameon=False, loc='upper left', ncol=2, handlelength=4)
-
-    title = plot_cfg.get("title", "")
-    ax.set_title(title)
-
-    ax.set_xlabel('')
-    ax.yaxis.set_major_formatter(ScalarFormatter())
-    ax.yaxis.major.formatter._useMathText = True
-    ax.yaxis.set_minor_locator(AutoMinorLocator(5))
-    ax.xaxis.set_minor_locator(AutoMinorLocator(5))
-    ax.yaxis.set_label_coords(0.63, 1.01)
-    ax.yaxis.tick_right()
-
-    return fig
 
 def generate_figure(plot_cfg, root_dir):
 
+
     fig = plt.figure()
+    fig.set_tight_layout(True)
+    fig.autofmt_xdate()
 
-
-    configure_plot(fig)
-
-    if "generator" in plot_cfg:
-        if plot_cfg["generator"] == "regplot":
-            fig = generator_regplot(fig, root_dir, plot_cfg)
-        else:
-            print("Unhandled generator!")
-            assert False
-    else:
-        fig = generator_errorbar(fig, root_dir, plot_cfg)
+    fig = generator_bar(fig, root_dir, plot_cfg)
 
     return fig
 
-def generator_errorbar_deps(yaml_dir, plot_cfg):
-    deps = ""
-    
-    for s in plot_cfg["series"]:
-        file_path = s["file"]
-        if not os.path.isabs(file_path):
-            file_path = os.path.join(yaml_dir, file_path)
-        deps += file_path + " "
 
-    return deps
+# Make some style choices for plotting
+color_wheel = ['#329932',
+               '#ff6961',
+               'b',
+               '#6a3d9a',
+               '#fb9a99',
+               '#e31a1c',
+               '#fdbf6f',
+               '#ff7f00',
+               '#cab2d6',
+               '#6a3d9a',
+               '#ffff99',
+               '#b15928',
+               '#67001f',
+               '#b2182b',
+               '#d6604d',
+               '#f4a582',
+               '#fddbc7',
+               '#f7f7f7',
+               '#d1e5f0',
+               '#92c5de',
+               '#4393c3',
+               '#2166ac',
+               '#053061']
+dashes_styles = [[3, 1],
+                [1000, 1],
+                [2, 1, 10, 1],
+                [4, 1, 1, 1, 1, 1]]
 
-def generate_deps(plot_cfg, root_dir):
 
-    if "generator" in plot_cfg:
-        print("Unhandled generator!")
-        assert False
-    else:
-        deps = generator_errorbar_deps(root_dir, plot_cfg)
+# ===========================================================
+# Directory and filename; style file open
+# ===========================================================
+def configure_plot():
+    dirFile = os.path.dirname(os.path.realpath(__file__))
+    # Load style file
+    plt.style.use(os.path.join(dirFile, 'PaperDoubleFig.mplstyle'))
 
-    return deps
+
 
 if __name__ == '__main__':
 
@@ -199,13 +172,11 @@ if __name__ == '__main__':
         cfg = yaml.load(f)
 
 
-    if "--deps" in sys.argv[1:]:
-        deps_str = generate_deps(cfg, root_dir)
-        print(output_path, ":", deps_str)
-    else:
-        fig = generate_figure(cfg, root_dir)
+    configure_plot()
 
+    fig = generate_figure(cfg, root_dir)
+    if fig is not None:
         # Save plot
         print("saving to", output_path)
-        fig.savefig(output_path, bbox_inches="tight",
-                    clip_on=False, transparent=True)
+        fig.show()
+        fig.savefig(output_path,  clip_on=False, transparent=True)

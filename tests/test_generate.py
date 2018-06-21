98ab38622dd1fae303b284478aa9b73b707128d5
Carl Pearson <carl.w.pearson@gmail.com>
2018-06-21 09:16:25 -0500

    search __fixtures directory for data files in test_generate.py

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -6,16 +6,17 @@ FIXTURES_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "
 
 def generate_fixture(name):
     figure_spec = spec.load(os.path.join(FIXTURES_DIR, name))
+    figure_spec = spec.use_search_dirs(figure_spec, [FIXTURES_DIR])
     fig = figure.generate(figure_spec)
 
-def test_errorbar():
+def test_generate_errorbar():
     generate_fixture("errorbar.yml")
 
-def test_regplot():
+def test_generate_regplot():
     generate_fixture("regplot.yml")
 
-def test_bar():
+def test_generate_bar():
     generate_fixture("bar.yml")
 
-def test_subplots():
+def test_generate_subplots():
     generate_fixture("subplots.yml")

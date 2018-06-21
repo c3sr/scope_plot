300d75299572e2cd93c2949aee7b7c5681505091
Carl Pearson <carl.w.pearson@gmail.com>
2018-06-21 09:15:40 -0500

    add test for using search directories in figure spec

---
+++ tests/test_use_search_dirs.py
@@ -0,0 +1,10 @@
+import os
+from microbench_plot import spec
+from microbench_plot import figure
+
+FIXTURES_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "__fixtures")
+
+def test_use_search_dirs():
+    figure_spec = spec.load(os.path.join(FIXTURES_DIR, "errorbar.yml"))
+    figure_spec = spec.use_search_dirs(figure_spec, [FIXTURES_DIR])
+    assert figure_spec["series"][0]["file"].startswith(FIXTURES_DIR)

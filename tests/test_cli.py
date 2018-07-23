c909980e4fbcc35652562a99d9c997e14a66841b
Carl Pearson <pearson@illinois.edu>
2018-07-23 13:57:29 -0500

    add merge cli test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -5,6 +5,7 @@ from scope_plot import cli
 
 pytest_plugins = ["pytester"]
 
+FIXTURES_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "__fixtures")
 
 @pytest.fixture
 def run(testdir):
@@ -13,19 +14,14 @@ def run(testdir):
         return testdir._run(*args)
     return do_run
 
-
 def test_scope_plot_help(tmpdir, run):
     result = run("--help")
     assert result.ret == 0
 
-# def test_scope_plot_version(tmpdir, run):
-#     input = tmpdir.join("example.txt")
-#     content = unicode("\xc3\xa4\xc3\xb6", "latin1")
-#     with input.open("wb") as f:
-#         f.write(content.encode("latin1"))
-#     output = tmpdir.join("example.txt.utf8")
-#     result = run("-flatin1", "-tutf8", input, "-o", output)
-#     assert result.ret == 0
-#     with output.open("rb") as f:
-#         newcontent = f.read()
-#     assert content.encode("utf8") == newcontent
+def test_scope_plot_version(tmpdir, run):
+    result = run("version")
+    assert result.ret == 0
+
+def test_scope_plot_merge(tmpdir, run):
+    result = run("merge", FIXTURES_DIR)
+    assert result.ret == 0
